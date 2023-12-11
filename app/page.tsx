44bbca409b816af401f9a11c345d95d36c35824f
Andrew <andrewjsides@gmail.com>
2023-12-10 20:14:16 -0800

    Added resume download, updated text

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
 import { SocialIcon } from "react-social-icons";
-import { useState } from "react";
+import { AiOutlineDownload } from "react-icons/ai";
+import { useState, useEffect } from "react";
 
 export default function Home() {
   const [selected, setSelected] = useState("bio");
@@ -26,6 +28,11 @@ export default function Home() {
       name: "Projects",
       key: "landing",
       render: () => Projects(),
+    },
+    {
+      name: "Interests",
+      key: "interests",
+      render: () => Interests(),
     },
     {
       name: "Contact Me",
@@ -55,15 +62,20 @@ export default function Home() {
             Larissa Sides
           </div>
         </div>
-        <div className="text-lg w-1/4 pt-24 text-center text-slate-800">
+        <div className="text-md w-[35%] pt-20 text-slate-800 pr-4">
           <div className="text-3xl text-black">About Me</div>
           <br />
-          Experienced Emergency Medical Technician with a demonstrated history
-          of working in the higher education industry. Skilled in Nutrition
-          Education, First Aid, Diet Planning, Medical Terminology, and Basic
-          Life Support (BLS). Strong healthcare services professional with a
-          Bachelor of Science - BS focused in Nutrition from Arizona State
-          University.{" "}
+          Hello! I am Larissa Sides, and I am a dedicated and skilled Registered
+          Nurse with a Bachelor of Science in Nursing. Currently, I am pursuing
+          my FNP/DNP. My background is comprehensive, encompassing nursing,
+          nutrition, and emergency medical care, bringing over eight years of
+          experience in various medical settings. My expertise spans oncology
+          nursing and critical care in ICU environments to teaching future EMTs.
+          My strengths lie in handling high-stress situations with composure,
+          proficiency in advanced medical procedures and equipment, and a deep
+          commitment to providing empathetic patient care. My education and work
+          history are a testament to my continuous journey of professional
+          development and my unwavering dedication to the healthcare field.{" "}
         </div>
       </>
     );
@@ -75,7 +87,7 @@ export default function Home() {
         <header className="text-black text-2xl text-center text-[#FAF2F0] h-fit whitespace-nowrap ">
           Mission Statement
         </header>
-        <div className="p-8 text-lg text-slate-800">
+        <div className="p-12 text-lg text-slate-800">
           {" "}
           I&apos;m a passionate healthcare professional dedicated to delivering
           top-notch patient care.
@@ -100,15 +112,23 @@ export default function Home() {
     return (
       <>
         <div className="text-xl text-[#FAF2F0] text-center mt-8 ml-4">
+          <a
+            download
+            href="/Larissa Sides_Resume_12102023.pdf"
+            className="mb-8 flex flex-row gap-2 items-center text-blue-900 font-bold text-2xl hover:underline hover:text-blue-700"
+          >
+            Download Resume
+            <AiOutlineDownload />
+          </a>
           <table className="table-auto">
             <tbody>
               <tr>
-                <td className="text-3xl text-black">Work Experience</td>
+                <td className="text-2xl text-black">Work Experience</td>
               </tr>
               <tr className="flex flex-row items-center space-x-4 mb-4">
                 <td>
                   <Image
-                    className="rounded-3xl border-2 border-black/80"
+                    className="rounded-2xl border-2 border-black/80"
                     src="/sutter.jpg"
                     alt="Picture of the author"
                     width={40}
@@ -278,6 +298,45 @@ export default function Home() {
     );
   };
 
+  const Interests = () => {
+    return (
+      <div className="flex flex-col justify-center text-center items-center">
+        <header className="text-black text-2xl text-center text-[#FAF2F0] h-fit whitespace-nowrap ">
+          Interests
+        </header>
+        <div className="p-12 text-lg text-slate-800">
+          As a healthcare professional deeply invested in the medical field, my
+          interests align closely with my career and personal growth. I am
+          passionate about emergency medicine and the intricacies of critical
+          care, particularly in ICU settings. This interest is driven by a
+          desire to understand complex medical conditions and provide
+          compassionate, specialized care to those in acute need.
+          {<br />}
+          Education is another area of interest for me. I find great fulfillment
+          in imparting knowledge to future medical professionals, particularly
+          EMTs. This passion for teaching is not just about sharing knowledge,
+          but also about staying updated with the latest developments in
+          emergency medical procedures and instructional methods. My pursuit of
+          an FNP/DNP degree reflects my commitment to continuous learning and
+          professional development. I am always eager to expand my skills and
+          knowledge, especially in areas that directly impact patient care and
+          treatment outcomes. In addition to these professional interests, I
+          have a background in nutrition, indicating a keen interest in the
+          holistic approach to health and wellness. Understanding the role of
+          nutrition in overall health is crucial for me, both professionally and
+          personally. Finally, I thrive in high-stress environments, indicating
+          an interest in roles and situations that challenge my problem-solving
+          skills and composure. This trait is vital in the fast-paced,
+          ever-changing landscape of healthcare and emergency medicine. In
+          summary, my interests revolve around emergency medicine, critical
+          care, education, continuous learning, holistic health, and thriving in
+          challenging environments, all of which shape my approach to healthcare
+          and personal development.
+        </div>
+      </div>
+    );
+  };
+
   return (
     <main className="flex flex-row h-screen justify-between bg-[#FAF2F0] text-black font-bold">
       <div className="z-10 max-h-5xl text-center h-screen w-1/4 items-center justify-between font-mono text-sm lg:flex">
@@ -286,12 +345,15 @@ export default function Home() {
             {items.map((item) => (
               <button
                 key={item.key}
-                className={`text-center cursor-pointer border-b-2 border-black/10 hover:border-black/50`}
+                className={`text-center cursor-pointer border-b-2 hover:border-black/50 border-black/10`}
                 onClick={() => handleItemClick(item.key)}
               >
                 {item.name}{" "}
                 {selected == item.key && (
                   <span className="text-red-500">*</span>
+                )}
+                {selected == item.key && (
+                  <div className="relative w-full border-b-2 border-black" />
                 )}
               </button>
             ))}
